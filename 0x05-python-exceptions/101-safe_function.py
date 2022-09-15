3eedf5095704faf3f6665baa4a31996bd23973f9
Alex kariuki <alexkamunge@gmail.com>
2022-09-15 07:15:05 -0700

    python exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        except Exception as i:
+            sys.stderr.write("Exception: {}\n".format(i))
+            result = None
+        return (None)
